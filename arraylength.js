f9b74e3540e9e1ae74e8d94f727ef700594bb309
Jahidul-Bin-Rafiq <jahidulrafiq@gmail.com>
2020-08-05 22:31:59 +0600

    update arraylength

--- arraylength.js
+++ arraylength.js
@@ -1,5 +1,6 @@
-// let element=[1, 2, 3, 4, 5, 6];
-// let result=element.join("");
-// console.log(result.length);
+//count the array length without empty string
+let element=[1, 2, 3, 4, 5, 6,""];
+let result=element.join("");
+console.log(result.length);
+// console.log(element.length);
 
-console.log(5**5);
