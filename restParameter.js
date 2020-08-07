945cd119fabf4476438bc71b62761b1e25f7604d
Jahidul-Bin-Rafiq <jahidulrafiq@gmail.com>
2020-08-07 10:28:54 +0600

    Rest Parameter example

---
+++ restParameter.js
@@ -0,0 +1,9 @@
+//rest parameters collects all the remaning elements
+
+function message(para1,para2,...para3){
+    console.log(para1,' ',para2);
+    console.log(para3.length);
+    console.log(para3[0]);
+    console.log(para3);
+}
+message("hey", "hello", "good", "goodmorning", "hi", "welldone");
