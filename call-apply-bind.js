018f0620f8612fd59308463ba166d3b59b018404
Jahidul-Bin-Rafiq <jahidulrafiq@gmail.com>
2020-08-11 11:45:39 +0600

    call-apply-bind

---
+++ call-apply-bind.js
@@ -0,0 +1,40 @@
+const person={
+    firstName:'rahim',
+    'last Name':'Khan',
+    salary:50000,
+    getFullName(){
+        console.log(`${this.firstName} ${this["last Name"]}`)
+    },
+    chargeBill(amount,tips,tax){
+          console.log(this);
+          this.salary=this.salary-amount-tips-tax;
+          return (this.salary)
+    }
+};
+const heroPerson={
+    firstName:'hero',
+    lastName:'alom',
+    salary:20000
+};
+const anotherPerson={
+    firstName:'sakib',
+    lastName:'khan',
+    salary:15000
+};
+//bind==>return a function then we set arguments
+/*
+const chargeBillHero=person.chargeBill.bind(heroPerson);
+console.log(chargeBillHero(2000));
+*/
+
+//call directly work in this method it directly called
+/*
+person.chargeBill.call(heroPerson,2000,300,23)
+console.log(heroPerson.salary);
+*/
+
+
+//apply passed argument as array from the second argument
+
+person.chargeBill.apply(heroPerson,[2000,300,23])
+console.log(heroPerson.salary);
