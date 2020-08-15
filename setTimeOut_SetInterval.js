3d16042dc402d05f498987c2d7a4bea3f4a58b97
Jahidul-Bin-Rafiq <jahidulrafiq@gmail.com>
2020-08-15 21:07:20 +0600

    add setTimeOut & SetInterval example

---
+++ setTimeOut_SetInterval.js
@@ -0,0 +1,22 @@
+        // function doSomething(){
+        //     console.log('using of setTimeOut');
+        // }
+        console.log(2222);
+        // setTimeout(function(){
+        //     console.log('using of setTimeOut');
+        // },5000);
+        let count=0;
+        setInterval(() => {
+            let value=10;
+            count+=value;
+            console.log(`complete rate: ${count}`)
+            
+        }, 1000);
+        console.log(4444);
+        console.log(5555);
+        console.log(6666);
+        console.log(7777);
+        console.log(6666);
+        console.log(7777);
+        console.log(6666);
+        console.log(7777);
