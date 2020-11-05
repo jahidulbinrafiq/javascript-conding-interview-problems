e37ccda9f0c83370254f62e383ca35da26df4f24
Jahidul Bin Rafiq <jahidulrafiq@gmail.com>
2020-11-05 12:44:22 +0600

    Calculate Factorial of a number

--- factorial_Iteration.js
+++ factorial_Iteration.js
@@ -4,24 +4,28 @@
  * Con: The code is lengthier than that of the recursive implementation
  */
 
-
-    /**  
+/**  
         let factorial=1;//intial value
+        
         let num=4;//find the factorial of 4
+        
         for (let i=num;i>=1;i--){
+        
         console.log(factorial," X",i,"=",factorial*i)
+        
         factorial*=i;
 
         }
         
         console.log(factorial); 
 
-  */
+ */
 
 
 //making it for  multiple value
 
 function factorialIteration(value){
+    
     let answer=1;
     if(value==0 || value==1){
         return answer;
