a16d628ab2d9b8839ce6eb1abe59aecbd1d06d23
Jahidul Bin Rafiq <jahidulrafiq@gmail.com>
2020-11-05 12:13:40 +0600

    Remove duplicate item from an array

--- removeDuplicate.js
+++ removeDuplicate.js
@@ -1,10 +1,21 @@
-let id=[1,3,5,67,40,45,80,100,4,3,2,1,100,30,34,56,46];
-let filterList=[];
-for(let i=0;i<id.length;i++){
-    let element=id[i];
-    let index=filterList.indexOf(element);
-    if(index==-1){
-        filterList.push(element);
+// let id=[1,3,5,67,40,45,80,100,4,3,2,1,100,30,34,56,46];
+// let filterList=[];
+// for(let i=0;i<id.length;i++){
+//     let element=id[i];
+//     let index=filterList.indexOf(element);
+//     if(index==-1){
+//         filterList.push(element);
+//     }
+// }
+// console.log(filterList);
+
+
+
+let marks=[10,30,35,45,67,89,90,100,99,99,5,5,101];
+let uniqueValue=[]
+for (const iterator of marks) {
+    if(uniqueValue.indexOf(iterator)===-1){
+        uniqueValue.push(iterator)
     }
 }
-console.log(filterList);
+console.log(uniqueValue)
