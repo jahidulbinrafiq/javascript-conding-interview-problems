8923f46a2fad98c37d3022e027c2c9d8bcff1d8c
Jahidul Bin Rafiq <jahidulrafiq@gmail.com>
2020-11-05 11:59:47 +0600

    Sum of all numbers in an array

--- arraySum.js
+++ arraySum.js
@@ -1,6 +1,17 @@
 let marks=[10,30,35,45,67,89,90,100,99,99,5,101];
 let sum=0;
-for(let i=0;i<marks.length;i++){
-    sum+=marks[i];
-}
-console.log(sum);
+// for(let i=0;i<marks.length;i++){
+//     sum+=marks[i];
+// }
+// console.log(sum);
+
+// for...of iterates over property values===>that means It can be used in array
+
+//using for of loop to find the sum of all numbers in an array
+
+/*
+for (const iterator of marks) {
+     sum+=iterator
+ }
+ console.log('Total: ',sum)
+*/
