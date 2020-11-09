5794e9a3cc273af5794e84cc3f3599ed4e344343
Jahidul-Bin-Rafiq <jahidulrafiq@gmail.com>
2020-11-09 10:15:13 +0600

    Global and Local Scope with let and const (ES6)

---
+++ scope.js
@@ -0,0 +1,35 @@
+// Global Scope
+var a = 1;
+let b = 2;
+const c = 3;
+
+
+console.log(`Global Scope: `, a, b, c);
+
+function test() {
+    var a = 4;
+    let b = 5;
+    const c = 6;
+    console.log(`Functions Scope: `, a, b ,c);
+}
+
+test();
+
+console.log(`Global Scope: `, a, b, c);
+
+if (true) {
+    var a = 7;
+    let b = 8;
+    const c = 9;
+    console.log(`If Scope: ` , a, b, c);
+}
+
+console.log(`Global Scope: `, a, b, c);
+
+
+for (let a = 0; a <10; a ++) {
+    console.log(`Loop: `, a);
+}
+
+
+console.log(`Global Scope: `, a, b, c);
