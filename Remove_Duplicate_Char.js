57f3e3bb27ea3ca4d177fa43bd7e3e60f2f1c6f6
Jahidul-Bin-Rafiq <jahidulrafiq@gmail.com>
2021-01-08 08:54:57 +0600

    remove duplicate char from string

--- Remove_Duplicate_Char.js
+++ Remove_Duplicate_Char.js
@@ -1,9 +1,35 @@
 const str='Hello jamall kah';
+
+//way 1:
+/*
+let str1='';
+for(let index=0;index<str.length;index++) {
+    if(str1.indexOf(str[index])==-1){
+        str1+=str[index];
+    }
+    str1+=' ';
+}
+console.log(str1);
+*/
+
+//way 2
+/*
 const result =str.split(' ').join('')
 const str1=[]
 for (const iterator of result) {
     if(str1.indexOf(iterator)==-1){
         str1.push(iterator)
-    } 
+    }
 }
 console.log(str1.join(''))
+*/
+
+//way 3
+
+let str1=''
+for (const index in str){
+    if(str1.indexOf(str[index])==-1 || str[index]==' '){
+        str1+=str[index]
+    }
+}
+console.log(str1)
